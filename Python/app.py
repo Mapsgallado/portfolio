625961ed7781d9001a03e00275a4fb84d4958299
Mapsgallado <240032027+Mapsgallado@users.noreply.github.com>
2025-12-04 23:42:47 +0200

    feat: create app.py

---
+++ Python/app.py
@@ -0,0 +1,21 @@
+# print ("Papamaps")
+# print ("o----")
+# print (' !!!!') 
+# print ('*' * 10) # an expression, meaning when we run the code it will express a value.
+
+# price = 10
+# price = 20 #integer, number without out a decimal
+# print (price)
+
+# name = 'John Smith'
+# age = 20
+# new_patient = True
+
+# print (name)
+# print (age)
+# print (new_patient)
+
+name = input('Whats your name? ')
+color = input('Whats your favorite color ')
+
+print (name + ' likes ' + color)
